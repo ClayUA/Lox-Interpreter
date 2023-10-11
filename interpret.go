b4ac80cbde0c72d5daa1703de6f29077ed9b02c9
ClayUA <claytruelove@gmail.com>
2023-10-11 14:28:43 -0500

    adding token types

---
+++ interpret.go
@@ -0,0 +1,53 @@
+package main
+
+type TokenType int
+
+const (
+	// single character tokens
+	LEFT_PAREN = iota
+	RIGHT_PAREN
+	LEFT_BRACE
+	RIGHT_BRACE
+	COMMA
+	DOT
+	MINUS
+	PLUS
+	SEMICOLON
+	SLASH
+	STAR
+
+	//one or more character tokens
+	BANG
+	BANG_EQUAL
+	EQUAL
+	EQUAL_EQUAL
+	GREATER
+	GREATER_EQUAL
+	LESS
+	LESS_EQUAL
+
+	//literals
+	NUMBER
+	STRING
+	IDENTIFIER
+
+	//keywords
+	AND
+	CLASS
+	ELSE
+	FALSE
+	FOR
+	FUN
+	IF
+	NIL
+	OR
+	PRINT
+	RETURN
+	SUPER
+	THIS
+	TRUE
+	VAR
+	WHILE
+
+	EOF
+)
